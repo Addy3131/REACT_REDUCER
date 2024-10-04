2df12d899f82a9e4afe9723dc5606f67f9a1eb30
Addy3131 <adnankureshi15@gmail.com>
2024-10-05 00:43:18 +0530

    first commit

---
+++ src/UseReducer.js
@@ -0,0 +1,73 @@
+import React,{ useReducer} from 'react';
+
+const initialState ={
+  data:[],
+  loading:false,
+  error:null
+}
+
+function reducer(state,action){
+  console.log(state)
+  console.log(action)
+  switch(action.type){
+    case 'FETCH_DATA_START':
+      return{...state,loading:true};
+
+    case 'FETCH_DATA_SUCCESS':
+      return{loading:false,error:null,data:action.payload};
+
+    case 'FETCH_DATA_FAILURE':
+      return{...state,loading:false,error:action.payload};
+
+    case 'DELETE_DATA':
+      return{...state,data:state.data.filter((item)=>(item.id !== action.payload))};
+
+    case 'ADD_DATA':
+        return{...state,data:[...state.data,action.payload]};
+  }
+}
+const useReducerEx = () => {
+  const [state,dispatch]=useReducer(reducer,initialState);
+    const fetchdata  = async () => {
+        dispatch({type : 'FETCH_DATA_START'});
+        try{
+          const response =await fetch('https://jsonplaceholder.typicode.com/posts')
+          const data = await response.json();
+          dispatch({type : 'FETCH_DATA_SUCCESS',payload:data})
+          console.log(data)
+        }
+        catch(error){
+          dispatch({type : 'FETCH_DATA_FAILURE',payload:error.message})
+        }
+    }
+    const deleteData =(id)=>{
+      dispatch({type : 'DELETE_DATA',payload:id})
+    }
+    const addData =(newData)=>{
+      dispatch({type : 'ADD_DATA',payload:newData})
+    }
+  return (
+    <div>
+      <button onClick={fetchdata}>Fetch Data</button>
+      {state.loading && <p>Loading</p>}
+      {state.error && <p>Error:{state.error}</p>}
+      <ul>
+        {state.data.map((item)=>(
+        <li key={item.id}>{item.title}{' '} <button onClick={()=>deleteData(item.id)}>Delete</button></li>
+        ))}
+      </ul>
+      <form onSubmit={(e)=>{
+        e.preventDefault();
+        addData({
+          id:Date.now(),
+          title:e.target.title.value
+        })
+      }}>
+        <input type='text' name='title' placeholder='Add new Title'></input>
+        <button type='submit'>Add</button>
+      </form>
+    </div>
+  )
+}
+
+export default useReducerEx
